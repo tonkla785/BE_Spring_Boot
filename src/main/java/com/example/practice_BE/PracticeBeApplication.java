e6206f599d4b23aa537fde5c0d178a0ddea44a58
tonkla785 <tonkla785@gmail.com>
2025-07-11 00:44:51 +0700

    Create spring boot

---
+++ src/main/java/com/example/practice_BE/PracticeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_BE/PracticeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
